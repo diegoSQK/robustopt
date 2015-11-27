85acb0afcdd41e80cc9fc7b439cab135582e148e
Diego Kiner <Diego@Diegos-MacBook-Pro.local>
2015-11-26 16:25:11 -0800

    Modified simulation script

--- simulate_opt.m
+++ simulate_opt.m
@@ -2,14 +2,21 @@ function [regret, wealth] = simulate_opt(policy, horizon, order)
 % Inputs: 
 %   policy : function handle to optimization function
 %   horizon : Planning horizon for optimization
-%   order : Order of autoregressive model for uncertainty set recovery
+%   order : Order of autoregressive model for uncertainty set recovery.
+%           (Optional argument, naive method will be used if not specified)
 % Outputs: 
 %   wealth : Actual terminal wealth
 %   regret : Percentage distance from true optimality
 
     [past_pred, past_real] = generate_demand(100);
     past_err = past_pred - past_real;
-    B = uncertainty(past_err, order, horizon);
+    
+    if nargin > 2
+        B = uncertainty_auto(past_err, order, horizon);
+    else
+        B = uncertainty_naive(past_real, horizon);
+    end
+
     [A, b_real, b_hat, err_signal] = initialization(horizon, horizon);
 
     [x_opt, y_opt, z_opt] = naive_opt(A, b_real);
